783091e5bb199afd54378bac82518244799b3c5e
GustavoTZ14 <gterrin155@gmail.com>
2024-10-07 17:45:32 -0300

    0710

---
+++ JavaScript/For_of.js
@@ -0,0 +1,17 @@
+let cars = ["BMW", "Volvo", "Mini"];
+
+let text = "";
+for (let x of cars) {
+  text += x;
+}
+
+console.log(text);
+
+let language = "JavaScript";
+
+let txt = "";
+for (let x of language) {
+  txt += x;
+}
+
+console.log(txt);
---
+++ JavaScript/While.js
@@ -0,0 +1,10 @@
+let text = "";
+
+let i = 0;
+
+while (i < 10) {
+  text += "the number is " + i + "\n";
+  i++;
+}
+
+console.log(text);
